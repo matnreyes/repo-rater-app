14e491e37f27eb0d8118894ac82af9efcc3c716d
matnreyes <matias_reyes@icloud.com>
2023-03-06 20:29:39 -0500

    add styling to app

---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+import Constants from 'expo-constants'
+import { StyleSheet, View } from 'react-native'
+import Text from './Text'
+import RepositoryList from './RepositoryList'
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1
+  }
+})
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text fontWeight="bold" color="textPrimary">Rate Repository Application</Text>
+      <RepositoryList />
+    </View>
+  )
+}
+
+export default Main
